cae78f2785f621f494c4e70b9f546a2aaca1d1b3
とみ <tominari20030822@icloud.com>
2024-11-19 10:34:07 +0900

    アルバイトとは別にイベントを追加するやつ作りました。でもまだ見た目には出てこないです

---
+++ Calendar17/Event+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Event+CoreDataClass.swift
+//  Calendar17
+//
+//  Created by 冨成 祐羽 on 2024/11/19.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Event)
+public class Event: NSManagedObject {
+
+}
---
+++ Calendar17/Event+CoreDataProperties.swift
@@ -0,0 +1,35 @@
+//
+//  Event+CoreDataProperties.swift
+//  Calendar17
+//
+//  Created by 冨成 祐羽 on 2024/11/19.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Event {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Event> {
+        return NSFetchRequest<Event>(entityName: "Event")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var eventDescription: String?
+    @NSManaged public var startDate: Date?
+    @NSManaged public var name: String?
+    @NSManaged public var endDate: Date?
+    @NSManaged public var color: String?
+    @NSManaged public var isNotificationEnabled: Bool
+    @NSManaged public var notificationTime: Date?
+    @NSManaged public var memo: String?
+    @NSManaged public var isRecurring: Bool
+    @NSManaged public var recurrenceRule: String?
+
+}
+
+extension Event : Identifiable {
+
+}
